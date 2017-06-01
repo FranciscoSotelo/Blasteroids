3f2354911fbf79951bbbe86a04d956f680f06030
Jethro Aquino <jlim.aquino@gmail.com>
2017-06-01 05:07:02 -0700

    Added Menu and a bunch of other stuff

---
+++ U3DBlasteroids/Assets/SCRIPTS/FRONTEND/fxAnimationFadeYoyo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class fxAnimationFadeYoyo : MonoBehaviour
+{
+    public Text FadingText;
+    public float Duration;
+
+    void Start()
+    {
+        FadingText.DOFade(0f, Duration).SetEase(Ease.OutQuad).SetLoops(-1, LoopType.Yoyo);
+    }
+}
---
+++ U3DBlasteroids/Assets/SCRIPTS/FRONTEND/uiMenuSlider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class uiMenuSlider : MonoBehaviour
+{
+    public Transform SlidingObject;
+    public float SlidingResistance;
+
+    public float ScreenTransition12;    // x for screen 1 to 2 transition
+    public float ScreenTransition21;    
+    public float ScreenX1;              // x for screen 1
+    public float ScreenX2;              // x for screen 2
+
+    private int ScreenID = 1;
+
+    private Vector3 startPos;
+    private Vector3 startObjectPos;
+
+    public void OnDrag()
+    {
+        Vector3 curPosition = Input.mousePosition;
+        Vector3 difference = (curPosition - startPos)/SlidingResistance;
+        SlidingObject.position = new Vector3 (startObjectPos.x - difference.x, startObjectPos.y, startObjectPos.z);
+    }
+
+    public void OnClick()
+    {
+        startPos = Input.mousePosition;
+        startObjectPos = SlidingObject.position;
+        SlidingObject.DOKill(false);
+    }
+
+    public void OnRelease()
+    {
+        if (ScreenID == 1)
+        {
+            if (SlidingObject.position.x >= ScreenTransition12)
+            {
+                SlidingObject.DOMoveX(ScreenX2, 0.5f).SetEase(Ease.OutQuad).SetAutoKill(true);
+                ScreenID = 2;
+            }
+            else
+            {
+                SlidingObject.DOMoveX(ScreenX1, 0.5f).SetEase(Ease.OutQuad).SetAutoKill(true);
+                ScreenID = 1;
+            }
+        }
+        else if (ScreenID == 2)
+        {
+            if (SlidingObject.position.x <= ScreenTransition21)
+            {
+                SlidingObject.DOMoveX(ScreenX1, 0.5f).SetEase(Ease.OutQuad).SetAutoKill(true);
+                ScreenID = 1;
+            }
+            else
+            {
+                SlidingObject.DOMoveX(ScreenX2, 0.5f).SetEase(Ease.OutQuad).SetAutoKill(true);
+                ScreenID = 2;
+            }
+        }
+    }
+}
